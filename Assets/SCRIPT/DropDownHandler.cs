6a2fb512f0ac49ab55e9979cc824038085ef4dcf
Doroteo111 <ladoritaesta@gmail.com>
2023-11-17 22:01:26 +0100

    cositas

--- Assets/SCRIPT/DropDownHandler.cs
+++ Assets/SCRIPT/DropDownHandler.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class DropDownHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // NO ESTA ASIGANDO PORQUE NO ME DEJA LOL
+    
+    List<string> names = new List<string>() { "one,two,three, four" };
+    // referencia
+    public TMP_Dropdown dayDropDown;
+    public TMP_Text selectedName;
+
+
+    public void Dropdown_IndexChanged(int index)
     {
-        
+        //which name has been selected , what happend?
+        selectedName.text = names[index];
+    }
+    public void Start()
+    {   
+        DaysList();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+   public void DaysList()
+    {       
+        dayDropDown.AddOptions(names);
+        //var numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
     }
+    
 }
