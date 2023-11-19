5f3ee9605645d89dbea4c0caf1668771bce85a7c
Doroteo111 <ladoritaesta@gmail.com>
2023-11-19 19:33:18 +0100

    no puedo massss

--- Assets/SCRIPT/DropDownHandler.cs
+++ Assets/SCRIPT/DropDownHandler.cs
@@ -7,28 +7,128 @@ using TMPro;
 
 public class DropDownHandler : MonoBehaviour
 {
-    // NO ESTA ASIGANDO PORQUE NO ME DEJA LOL
-    
-    List<string> names = new List<string>() { "one,two,three, four" };
-    // referencia
+    // reference
     public TMP_Dropdown dayDropDown;
-    public TMP_Text selectedName;
+    public TMP_Dropdown monthDropDown;
+    public TMP_Dropdown yearDropDown;
+
+    public TMP_Text daySelectedName;
+    public TMP_Text monthSelectedName;
+    public TMP_Text yearSelectedName;
+
+    //Lists
+    List<string> dayNames = new List<string>() { "Plase select day","1", "2", "3", "4", "5", "6", "7", "8","9","10" };
+    List<string> monthNames = new List<string>() { "Plase select month", "jun","feb","march","april", "may" };
+    List<string> yearNames = new List<string>() { "Plase select year", "2010", "2016", "2036", "300" };
+
+    //son años bisiestos aquellos divisibles por 4
+    //tener en cuenta la fecha de febrero!!
 
 
-    public void Dropdown_IndexChanged(int index)
+    /* public void Dropdown_IndexChanged(int index)   
+     {
+         daySelectedName.text = dayNames[index];
+         monthSelectedName.text = monthNames[index];
+         yearSelectedName.text = yearNames[index];
+
+         if (index == 0)
+         {
+             daySelectedName.color = Color.red;
+             monthSelectedName.color= Color.red;
+             yearSelectedName.color = Color.red;
+         }
+         else
+         {
+             daySelectedName.color = Color.white;
+             monthSelectedName.color = Color.white;
+             yearSelectedName.color = Color.white;
+         }
+
+     } */  //solucioné este código porque me hacia movidas raísimas
+
+    //UI display, show if is chose or not
+   public void DayDropdown_IndexChanged(int index)
     {
-        //which name has been selected , what happend?
-        selectedName.text = names[index];
+        if (index == 0)
+        {
+            daySelectedName.color = Color.red;
+        }
+        else
+        {
+            daySelectedName.color = Color.white;
+        }
+    }
+   public void MonthDropdown_IndexChanged(int index)
+    {
+        if (index == 0)
+        {
+            monthSelectedName.color = Color.red;
+        }
+        else
+        {
+            monthSelectedName.color = Color.white;
+        }
+    }
+   public void YearDropdown_IndexChanged(int index)
+    {
+        if (index == 0)
+        {
+            yearSelectedName.color = Color.red;
+        }
+        else
+        {
+            yearSelectedName.color = Color.white;
+        }
     }
     public void Start()
     {   
         DaysList();
+        MounthsList();
+        YearsList();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SelectedText();
+        }
     }
 
-   public void DaysList()
+    public void DaysList()
     {       
-        dayDropDown.AddOptions(names);
-        //var numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        dayDropDown.AddOptions(dayNames);
     }
-    
+    public void MounthsList()
+    {
+      monthDropDown.AddOptions(monthNames);
+    }
+    public void YearsList()
+    {
+       yearDropDown.AddOptions(yearNames);
+    }
+
+    public void SelectedText()
+    {
+        //Debug.Log(monthDropDown.value);
+        //Debug.Log((dayDropDown.value),(monthDropDown.value),(yearDropDown.value));
+         Debug.Log(dayDropDown.value);
+         Debug.Log(yearDropDown.value);
+    }
+    /*private bool isValid()
+    {
+         if(result == 0)
+        {
+            Debug.Log("This date is valid");  
+            return true;
+
+        }
+        else
+        {
+            Debug.Log("This date is not valid");
+            return false;
+    }*/
+
+    ///NOTAS --> al parecer los dropDown solo aceptan strings, los ints no van
+    ///al hacer el debug solo me pilla el primer dropdown, aún así, si especifico cual de los 3
+  
 }
